89e9e594e3b89d1d14cc76ac79c7e8efe35caceb
Pavan Kumar V <pavankumarv124@gmail.com>
2025-12-03 10:31:05 +0530

    chore: sqlalchemy + alembic + env setup

---
+++ app/config/settings.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    pg_host: str
+    pg_user: str
+    pg_password: str
+    pg_db: str
+    pg_port: int
+
+    redis_host: str
+    redis_port: int
+    redis_password: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
