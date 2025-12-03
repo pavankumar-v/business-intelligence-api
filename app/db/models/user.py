672d7b50e0dc9d89d651aeb213f6fc11481e458a
Pavan Kumar V <pavankumarv124@gmail.com>
2025-12-03 13:51:12 +0530

    chore: restructure user tabel - reset migrations - uuid issue

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,14 +1,16 @@
 import datetime as dt
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+import uuid
+from sqlalchemy import UUID, Column, Integer, String, Boolean, DateTime
 from app.db.base import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username = Column(String, unique=True, index=True)
     region = Column(String)
     is_active_sub = Column(Boolean, default=False)
     department = Column(String)
     company_name = Column(String)
     signup_date = Column(DateTime, default=dt.datetime.utcnow)
+
