29ea20ab022959966e3f41811c59b64273c0f6f7
Pavan Kumar V <pavankumarv124@gmail.com>
2025-12-03 10:50:19 +0530

    chore: add properties for settings pg url and redis url

--- app/config/settings.py
+++ app/config/settings.py
@@ -12,6 +12,14 @@ class Settings(BaseSettings):
     redis_port: int
     redis_password: str
 
+    @property
+    def pg_url(self):
+        return f"postgresql+psycopg2://{self.pg_user}:{self.pg_password}@{self.pg_host}:{self.pg_port}/{self.pg_db}"
+
+    @property
+    def redis_url(self):
+        return f"redis://{self.redis_host}:{self.redis_port}"
+
     model_config = SettingsConfigDict(env_file=".env.local")
 
 settings = Settings()
