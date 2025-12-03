bff49c51b9b1d3726706ff4cc01c0dc19f5bb6c9
Pavan Kumar V <pavankumarv124@gmail.com>
2025-12-03 14:54:03 +0530

    chore: clean test code

--- app/main.py
+++ app/main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI
 from app.config.settings import settings
-from app.db.db import SessionLocal, get_session
 from app.db.models import User
 import uuid
 from app.rq.rq import queue
 from loguru import logger
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from rq import Retry
 from rq_dashboard_fast import RedisQueueDashboard
 dashboard = RedisQueueDashboard(settings.redis_url, "/rq")
@@ -17,29 +14,7 @@ app = FastAPI(
 
 app.mount("/rq", dashboard)
 
-def count_words_at_url(url):
-    """Just an example function that's called async."""
-    logger.info("Counting words at")
-    engine = create_engine(
-        settings.pg_url,
-        pool_size=10,
-        max_overflow=20,
-        future=True,   # modern 2.0 style if using SQLAlchemy 1.4+
-    )
 
-    # session factory
-    SessionLocal = sessionmaker(bind=engine, autoflush=True, autocommit=False, expire_on_commit=False)
-    session = SessionLocal()
-    new_user = User(
-        username=f"test{uuid.uuid4()}",
-        region="test",
-        is_active_sub=False,
-    )
-    session.add(new_user)
-    session.commit()
-    session.close()
-    
-    return len(url.split())
 
 @app.get("/")
 async def read_root():
