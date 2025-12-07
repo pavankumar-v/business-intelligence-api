2ebcc79b5328225416e601a2156f29421c99515d
Pavan Kumar V <pavankumarv124@gmail.com>
2025-12-07 13:31:34 +0530

    chore: add more metrics on dailt comapny metrics

--- app/models/daily_company_metrics.py
+++ app/models/daily_company_metrics.py
@@ -7,6 +7,9 @@ class DailyCompanyMetric(Base):
     __tablename__ = "daily_company_metrics"
 
     date = Column(Date, primary_key=True)
+    region = Column(String, primary_key=True)
+    highest_used_model = Column(String, primary_key=True)
+    least_used_model = Column(String, primary_key=True)
     company_name = Column(String, primary_key=True)
     total_cost = Column(Float, nullable=False)
     conversation_count = Column(Integer, nullable=False)
